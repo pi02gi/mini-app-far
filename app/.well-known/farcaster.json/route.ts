b14aa59cd9f418ea3408759b2885dcf2d92aff92
Adam <pi02gi@gmail.com>
2025-05-10 08:16:00 +0000

    add: accountAssociation to farcaster config

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -2,8 +2,19 @@ import { NextResponse } from "next/server";
 import { APP_URL } from "../../../lib/constants";
 
 export async function GET() {
+
+
+
   const farcasterConfig = {
     // TODO: Add account association
+ 
+  "accountAssociation": {
+    "header": "eyJmaWQiOjg2NzAzMywidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDQ0NzE5NDlhMzEwNjBlRWFmNTBDNjA3Qjk3RWQ1NjBkNzM3MjBCNTIifQ",
+    "payload": "eyJkb21haW4iOiJtaW5pLWFwcC1mYXIudmVyY2VsLmFwcCJ9",
+    "signature": "MHg2YmIxNDlhMTliYTFjZmViNDE4MzRhZjQxZjA0MjVmYzdlOWQ5MmUwY2I5ODQyMGQ4ZGQwZDEzNjRhOGY2ZTIwNTg2MDkxYTE3ZTEwZWE1YTFlOTBiYjg1NGZhZjYzZWJjZGNmYTY4N2UwYTJiMjJiZGZiMDliMWMzMTUzNzQ5MDFi"
+  },
+
+
     frame: {
       version: "1",
       name: "Monad Farcaster MiniApp Template",
